249283e61976ea45dffbcde6f7a9e964676892cc
Rajesh <rajeshboddapati37@gmail.com>
2022-10-10 10:01:58 +0530

    06-10-2022_project2_commit1

---
+++ 2DArray.js
@@ -0,0 +1,13 @@
+let twoD = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+console.log("Output of a 2D array:");
+for(let i=0;i<3;i++)
+{
+    for(let j=0;j<3;j++)
+    {
+        console.log(twoD[i][j]);
+    }
+}
